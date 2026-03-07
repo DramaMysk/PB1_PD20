206ed9a455beddc380b8f0d3675abd7a173735be
Sad Drama <justmysk@inbox.lv>
2026-03-07 10:19:39 +0200

    pievienots name pytest testi

--- tests/test_app.py
+++ tests/test_app.py
@@ -5,6 +5,7 @@ sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 
 import pytest
 from app import check_age
+from app import check_name
 
 def test_valid_age():
     rezultats = check_age("18")
@@ -18,3 +19,15 @@ def test_invalid_age():
     with pytest.raises(ValueError):
         check_age("abc")
     
+
+def test_valid_name():
+    rezultats = check_name("Bob")
+    assert rezultats == "Bob"
+
+def test_strip_name():
+    rezultats = check_name("  Bob   ")
+    assert rezultats == "Bob"
+
+def test_invalid_name():
+    with pytest.raises(ValueError):
+        check_name("123")
